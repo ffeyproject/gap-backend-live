73ce87adb9fab4f2e67d30126fffc019381ed6c4
ffeyproject <100005694+ffeyproject@users.noreply.github.com>
2025-07-10 10:08:32 +0700

    rev Migrate No memo Inspecting MklbBj

--- console/migrations/m250709_033248_add_no_memo_column_to_inspecting_mklbj_table.php
+++ console/migrations/m250709_033248_add_no_memo_column_to_inspecting_mklbj_table.php
@@ -3,7 +3,7 @@
 use yii\db\Migration;
 
 /**
- * Handles adding columns to table `{{%inspecting_mklbj}}`.
+ * Handles adding columns to table `{{%inspecting_mkl_bj}}`.
  */
 class m250709_033248_add_no_memo_column_to_inspecting_mklbj_table extends Migration
 {
@@ -12,7 +12,7 @@ class m250709_033248_add_no_memo_column_to_inspecting_mklbj_table extends Migrat
      */
     public function safeUp()
     {
-        $this->addColumn('{{%inspecting_mklbj}}', 'no_memo', $this->string()->null());
+        $this->addColumn('{{%inspecting_mkl_bj}}', 'no_memo', $this->string()->null());
     }
 
     /**
@@ -20,6 +20,6 @@ class m250709_033248_add_no_memo_column_to_inspecting_mklbj_table extends Migrat
      */
     public function safeDown()
     {
-        $this->dropColumn('{{%inspecting_mklbj}}', 'no_memo');
+        $this->dropColumn('{{%inspecting_mkl_bj}}', 'no_memo');
     }
 }
