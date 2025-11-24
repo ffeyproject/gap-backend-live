0b7eeb492998b417d7fb98fd7db7003c28445014
ffeyproject <100005694+ffeyproject@users.noreply.github.com>
2025-11-24 11:29:39 +0700

    Ganti panjang_m greige menjadi Float

---
+++ console/migrations/m251124_042603_change_panjang_m_pfp_to_double.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m251124_042603_change_panjang_m_pfp_to_double
+ */
+class m251124_042603_change_panjang_m_pfp_to_double extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute('ALTER TABLE trn_kartu_proses_pfp_item ALTER COLUMN panjang_m TYPE double precision');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->execute('ALTER TABLE trn_kartu_proses_pfp_item ALTER COLUMN panjang_m TYPE integer');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m251124_042603_change_panjang_m_pfp_to_double cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
