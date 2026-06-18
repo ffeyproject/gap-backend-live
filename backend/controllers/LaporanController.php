069d58abe5f1c860134a602f78f76f93b5f0b5bb
Ferry Gilang Ferdiana <ffey.project@gmail.com>
2026-06-18 16:07:09 +0700

    feat: add LaporanController and dynamic UI for processing manual production reports

--- backend/controllers/LaporanController.php
+++ backend/controllers/LaporanController.php
@@ -14,7 +14,6 @@ class LaporanController extends Controller
      * @return mixed
      */    public function actionPrintPersiapanGabungan()
     {
-        $this->layout = 'print';
         $params = Yii::$app->request->queryParams;
         $searchModelDyeing = new \common\models\ar\TrnKartuProsesDyeingSearch();
         $searchModelPfp = new \common\models\ar\TrnKartuProsesPfpSearch();
@@ -129,13 +128,55 @@ class LaporanController extends Controller
             'pagination' => false,
         ]);
         
-        return $this->render('print-persiapan-gabungan', [
+        $content = $this->renderPartial('print-persiapan-gabungan', [
             'dataProvider' => $dataProvider,
             'mcFilter' => $mcFilter,
             'shiftPagiFilter' => $shiftPagiFilter,
             'shiftSiangFilter' => $shiftSiangFilter,
             'tanggalFilter' => $tanggalFilter,
         ]);
+        
+        $title = 'LAPORAN HARIAN PERSIAPAN GABUNGAN';
+        $shiftStr = [];
+        if (!empty($shiftPagiFilter)) {
+            $shiftStr[] = $shiftPagiFilter . ' (Pagi)';
+        }
+        if (!empty($shiftSiangFilter)) {
+            $shiftStr[] = $shiftSiangFilter . ' (Siang)';
+        }
+        if (!empty($shiftStr)) {
+            $title .= ' SHIFT: ' . implode(' DAN ', $shiftStr);
+        }
+        
+        $pdf = new \kartik\mpdf\Pdf([
+            'mode' => \kartik\mpdf\Pdf::MODE_BLANK,
+            'format' => \kartik\mpdf\Pdf::FORMAT_A4,
+            'orientation' => \kartik\mpdf\Pdf::ORIENT_PORTRAIT,
+            'destination' => \kartik\mpdf\Pdf::DEST_BROWSER,
+            'content' => $content,
+            'cssInline' => '
+                .row {
+                    margin: 0px 0px 0px 0px !important;
+                    padding: 0px !important;
+                }
+                
+                .col-xs-1, .col-sm-1, .col-md-1, .col-lg-1, .col-xs-2, .col-sm-2, .col-md-2, .col-lg-2, .col-xs-3, .col-sm-3, .col-md-3, .col-lg-3, .col-xs-4, .col-sm-4, .col-md-4, .col-lg-4, .col-xs-5, .col-sm-5, .col-md-5, .col-lg-5, .col-xs-6, .col-sm-6, .col-md-6, .col-lg-6, .col-xs-7, .col-sm-7, .col-md-7, .col-lg-7, .col-xs-8, .col-sm-8, .col-md-8, .col-lg-8, .col-xs-9, .col-sm-9, .col-md-9, .col-lg-9, .col-xs-10, .col-sm-10, .col-md-10, .col-lg-10, .col-xs-11, .col-sm-11, .col-md-11, .col-lg-11, .col-xs-12, .col-sm-12, .col-md-12, .col-lg-12 {
+                    border:0;
+                    padding:5px 0 5px 0;
+                    margin-left:-0.00001;
+                }
+                body, .row, .col-xs-12 { background-color: #fff; color: #000; }
+                table { page-break-inside: auto; background-color: #fff; color: #000; }
+                tr { page-break-inside: avoid; page-break-after: auto; }
+            ',
+            'options' => ['title' => 'Laporan Harian Persiapan Gabungan'],
+            'methods' => [
+                'SetTitle' => $title,
+                'SetFooter' => ['{PAGENO}'],
+            ]
+        ]);
+        
+        return $pdf->render();
     }
 
     public function actionPersiapanGabungan()
@@ -639,6 +680,292 @@ class LaporanController extends Controller
             }
         }
         return $this->redirect(Yii::$app->request->referrer ?: ['persiapan-dyeing']);
+    }
+
+    public function actionRekapBulananProcessing()
+    {
+        $bulan = Yii::$app->request->get('bulan', date('n'));
+        $tahun = Yii::$app->request->get('tahun', date('Y'));
+        
+        $startDate = date('Y-m-d', strtotime("$tahun-$bulan-01"));
+        $endDate = date('Y-m-t', strtotime("$tahun-$bulan-01"));
+        
+        // Initial data structure
+        $data = [
+            'hisaka_total_produksi' => 0, 'hisaka_jml_masuk_packing' => 0, 'hisaka_proses_celup' => 0,
+            'hisaka_proses_dasar_celup' => 0, 'hisaka_proses_topping' => 0, 'hisaka_proses_level' => 0,
+            'hisaka_proses_stripping' => 0, 'hisaka_proses_relax' => 0, 'hisaka_proses_pfp' => 0,
+            'hisaka_hari_kerja' => 0, 'hisaka_total_batch' => 0, 'hisaka_rata_per_hari_batch' => 0,
+            'hisaka_rata_per_hari_kartu' => 0, 'hisaka_rata_per_hari_jumbo' => 0,
+            'fukushin_proses_continuous' => 0, 'fukushin_perbaikan_cuci_ulang' => 0,
+            'steamer_proses_redox' => 0, 'steamer_proses_disperse' => 0,
+            'padding_proses_dyeing' => 0, 'padding_proses_printing' => 0,
+            'stenter_produksi_stenter3' => 0, 'stenter_produksi_stenter4' => 0,
+            'stenter_produksi_stenter5' => 0, 'stenter_produksi_stenter6' => 0,
+            'stenter_setting_set_dyeing' => 0, 'stenter_setting_set_ulang_dyeing' => 0,
+            'stenter_setting_set_pfp' => 0, 'stenter_setting_set_ulang_pfp' => 0,
+            'stenter_setting_set_non_wr' => 0,
+            'stenter_resin_rf_dyeing' => 0, 'stenter_resin_rf_ulang_dyeing' => 0,
+            'stenter_resin_rf_ulang_murni' => 0, 'stenter_resin_setelah_perbaikan' => 0,
+            'stenter_resin_jet_black_ulang' => 0, 'stenter_resin_rf_printing' => 0,
+            'stenter_resin_rf_ulang_printing' => 0,
+            'packing_printing' => 0, 'packing_dyeing' => 0, 'packing_makloon' => 0,
+            'packing_total_packing' => 0, 'packing_hasil_kirim' => 0
+        ];
+        
+        $machineMap = \yii\helpers\ArrayHelper::index(\common\models\ar\MstMesinProses::find()->all(), 'nama_mesin');
+        $dyeingProcessMap = \yii\helpers\ArrayHelper::map(\common\models\ar\MstProcessDyeing::find()->all(), 'id', 'nama_proses');
+        $pfpProcessMap = \yii\helpers\ArrayHelper::map(\common\models\ar\MstProcessPfp::find()->all(), 'id', 'nama_proses');
+        
+        // 1. Process Dyeing Processes
+        $monthStr = sprintf('%04d-%02d-', $tahun, $bulan);
+        $qDyeing = \common\models\ar\KartuProcessDyeingProcess::find()
+            ->alias('kp')
+            ->innerJoin('trn_kartu_proses_dyeing kpd', 'kp.kartu_process_id = kpd.id')
+            ->where(['>=', 'kpd.status', \common\models\ar\TrnKartuProsesDyeing::STATUS_DELIVERED])
+            ->andWhere(['not', ['kpd.status' => \common\models\ar\TrnKartuProsesDyeing::STATUS_BATAL]])
+            ->andWhere(['like', 'kp.value', '"tanggal":"' . $monthStr])
+            ->with(['kartuProcess.trnKartuProsesDyeingItems'])
+            ->all();
+
+        $hisakaKartuIds = [];
+        $hisakaJumboKartuIds = [];
+
+        foreach ($qDyeing as $kp) {
+            $val = \yii\helpers\Json::decode($kp->value);
+            $noMesin = isset($val['no_mesin']) ? $val['no_mesin'] : null;
+            if (!$noMesin) continue;
+            
+            $modelMesin = isset($machineMap[$noMesin]) ? $machineMap[$noMesin]->model_mesin : '';
+            $processName = isset($dyeingProcessMap[$kp->process_id]) ? $dyeingProcessMap[$kp->process_id] : '';
+            
+            $panjang = 0;
+            if ($kp->kartuProcess && $kp->kartuProcess->trnKartuProsesDyeingItems) {
+                foreach ($kp->kartuProcess->trnKartuProsesDyeingItems as $item) {
+                    $panjang += $item->panjang_m;
+                }
+            }
+
+            if (stripos($modelMesin, 'Hisaka') !== false) {
+                $data['hisaka_total_produksi'] += $panjang;
+                $data['hisaka_total_batch'] += 1;
+                $hisakaKartuIds[$kp->kartu_process_id] = true;
+                if (stripos($modelMesin, 'Jumbo') !== false) {
+                    $hisakaJumboKartuIds[$kp->kartu_process_id] = true;
+                }
+
+                if (stripos($processName, 'Dyeing') !== false || stripos($processName, 'Celup') !== false) {
+                    $data['hisaka_proses_celup'] += $panjang;
+                }
+                if (stripos($processName, 'Dasar Celup') !== false) {
+                    $data['hisaka_proses_dasar_celup'] += $panjang;
+                }
+                if (stripos($processName, 'Toping') !== false && stripos($processName, 'Level') === false) {
+                    $data['hisaka_proses_topping'] += $panjang;
+                }
+                if (stripos($processName, 'Level') !== false) {
+                    $data['hisaka_proses_level'] += $panjang;
+                }
+                if (stripos($processName, 'Stripping') !== false || stripos($processName, 'Cuci H2O2') !== false) {
+                    $data['hisaka_proses_stripping'] += $panjang;
+                }
+                if (stripos($processName, 'Relax') !== false) {
+                    $data['hisaka_proses_relax'] += $panjang;
+                }
+            }
+            
+            if (stripos($modelMesin, 'Fukushin') !== false || stripos($modelMesin, 'Continuous') !== false) {
+                $data['fukushin_proses_continuous'] += $panjang;
+                if (stripos($processName, 'Cuci') !== false || stripos($processName, 'Ulang') !== false) {
+                    $data['fukushin_perbaikan_cuci_ulang'] += $panjang;
+                }
+            }
+            
+            if (stripos($modelMesin, 'Steamer') !== false) {
+                if (stripos($processName, 'Redox') !== false) $data['steamer_proses_redox'] += $panjang;
+                if (stripos($processName, 'Disperse') !== false) $data['steamer_proses_disperse'] += $panjang;
+            }
+            
+            if (stripos($modelMesin, 'Padding') !== false) {
+                $data['padding_proses_dyeing'] += $panjang; // Assuming Dyeing card = padding dyeing
+            }
+            
+            if (stripos($modelMesin, 'Stenter') !== false) {
+                if ($noMesin == 'Stenter 3') $data['stenter_produksi_stenter3'] += $panjang;
+                if ($noMesin == 'Stenter 4') $data['stenter_produksi_stenter4'] += $panjang;
+                if ($noMesin == 'Stenter 5') $data['stenter_produksi_stenter5'] += $panjang;
+                if ($noMesin == 'Stenter 6') $data['stenter_produksi_stenter6'] += $panjang;
+
+                if (stripos($processName, 'Setting') !== false) {
+                    if (stripos($processName, 'Ulang') !== false || stripos($processName, 'Perbaikan') !== false) {
+                        $data['stenter_setting_set_ulang_dyeing'] += $panjang;
+                    } else {
+                        $data['stenter_setting_set_dyeing'] += $panjang;
+                    }
+                }
+                if (stripos($processName, 'Resin') !== false || stripos($processName, 'RF') !== false) {
+                    if (stripos($processName, 'Ulang Murni') !== false) {
+                        $data['stenter_resin_rf_ulang_murni'] += $panjang;
+                    } elseif (stripos($processName, 'Ulang') !== false) {
+                        $data['stenter_resin_rf_ulang_dyeing'] += $panjang;
+                    } elseif (stripos($processName, 'Setelah Perbaikan') !== false) {
+                        $data['stenter_resin_setelah_perbaikan'] += $panjang;
+                    } elseif (stripos($processName, 'Jet Black Ulang') !== false || stripos($processName, 'Perbaikan JB') !== false) {
+                        $data['stenter_resin_jet_black_ulang'] += $panjang;
+                    } else {
+                        $data['stenter_resin_rf_dyeing'] += $panjang;
+                    }
+                }
+                if (stripos($processName, 'Non WR') !== false) {
+                    $data['stenter_setting_set_non_wr'] += $panjang;
+                }
+            }
+        }
+
+        // 2. Process PFP Processes
+        $qPfp = \common\models\ar\KartuProcessPfpProcess::find()
+            ->alias('kp')
+            ->innerJoin('trn_kartu_proses_pfp kpd', 'kp.kartu_process_id = kpd.id')
+            ->where(['>=', 'kpd.status', \common\models\ar\TrnKartuProsesPfp::STATUS_DELIVERED])
+            ->andWhere(['not', ['kpd.status' => \common\models\ar\TrnKartuProsesPfp::STATUS_GAGAL_PROSES]])
+            ->andWhere(['like', 'kp.value', '"tanggal":"' . $monthStr])
+            ->with(['kartuProcess.trnKartuProsesPfpItems'])
+            ->all();
+
+        foreach ($qPfp as $kp) {
+            $val = \yii\helpers\Json::decode($kp->value);
+            $noMesin = isset($val['no_mesin']) ? $val['no_mesin'] : null;
+            if (!$noMesin) continue;
+            
+            $modelMesin = isset($machineMap[$noMesin]) ? $machineMap[$noMesin]->model_mesin : '';
+            $processName = isset($pfpProcessMap[$kp->process_id]) ? $pfpProcessMap[$kp->process_id] : '';
+            
+            $panjang = 0;
+            if ($kp->kartuProcess && $kp->kartuProcess->trnKartuProsesPfpItems) {
+                foreach ($kp->kartuProcess->trnKartuProsesPfpItems as $item) {
+                    $panjang += $item->panjang_m;
+                }
+            }
+
+            if (stripos($modelMesin, 'Hisaka') !== false) {
+                $data['hisaka_total_produksi'] += $panjang;
+                $data['hisaka_proses_pfp'] += $panjang;
+                $data['hisaka_total_batch'] += 1;
+                $hisakaKartuIds[$kp->kartu_process_id . '_pfp'] = true;
+                if (stripos($modelMesin, 'Jumbo') !== false) {
+                    $hisakaJumboKartuIds[$kp->kartu_process_id . '_pfp'] = true;
+                }
+            }
+
+            if (stripos($modelMesin, 'Stenter') !== false) {
+                if ($noMesin == 'Stenter 3') $data['stenter_produksi_stenter3'] += $panjang;
+                if ($noMesin == 'Stenter 4') $data['stenter_produksi_stenter4'] += $panjang;
+                if ($noMesin == 'Stenter 5') $data['stenter_produksi_stenter5'] += $panjang;
+                if ($noMesin == 'Stenter 6') $data['stenter_produksi_stenter6'] += $panjang;
+
+                if (stripos($processName, 'Setting') !== false) {
+                    if (stripos($processName, 'Ulang') !== false) {
+                        $data['stenter_setting_set_ulang_pfp'] += $panjang;
+                    } else {
+                        $data['stenter_setting_set_pfp'] += $panjang;
+                    }
+                }
+            }
+        }
+        
+        // 3. Process Printing (if exists)
+        if (class_exists('\common\models\ar\KartuProcessPrintingProcess')) {
+            $printingProcessMap = \yii\helpers\ArrayHelper::map(\common\models\ar\MstProcessPrinting::find()->all(), 'id', 'nama_proses');
+            $qPrint = \common\models\ar\KartuProcessPrintingProcess::find()
+                ->alias('kp')
+                ->innerJoin('trn_kartu_proses_printing kpd', 'kp.kartu_process_id = kpd.id')
+                ->where(['>=', 'kpd.status', \common\models\ar\TrnKartuProsesPrinting::STATUS_DELIVERED])
+                ->andWhere(['like', 'kp.value', '"tanggal":"' . $monthStr])
+                ->with(['kartuProcess.trnKartuProsesPrintingItems'])
+                ->all();
+
+            foreach ($qPrint as $kp) {
+                $val = \yii\helpers\Json::decode($kp->value);
+                $noMesin = isset($val['no_mesin']) ? $val['no_mesin'] : null;
+                if (!$noMesin) continue;
+                
+                $modelMesin = isset($machineMap[$noMesin]) ? $machineMap[$noMesin]->model_mesin : '';
+                $processName = isset($printingProcessMap[$kp->process_id]) ? $printingProcessMap[$kp->process_id] : '';
+                
+                $panjang = 0;
+                if ($kp->kartuProcess && $kp->kartuProcess->trnKartuProsesPrintingItems) {
+                    foreach ($kp->kartuProcess->trnKartuProsesPrintingItems as $item) {
+                        $panjang += $item->panjang_m;
+                    }
+                }
+
+                if (stripos($modelMesin, 'Padding') !== false) {
+                    $data['padding_proses_printing'] += $panjang;
+                }
+                
+                if (stripos($modelMesin, 'Stenter') !== false) {
+                    if ($noMesin == 'Stenter 3') $data['stenter_produksi_stenter3'] += $panjang;
+                    if ($noMesin == 'Stenter 4') $data['stenter_produksi_stenter4'] += $panjang;
+                    if ($noMesin == 'Stenter 5') $data['stenter_produksi_stenter5'] += $panjang;
+                    if ($noMesin == 'Stenter 6') $data['stenter_produksi_stenter6'] += $panjang;
+
+                    if (stripos($processName, 'Resin') !== false || stripos($processName, 'RF') !== false) {
+                        if (stripos($processName, 'Ulang') !== false) {
+                            $data['stenter_resin_rf_ulang_printing'] += $panjang;
+                        } else {
+                            $data['stenter_resin_rf_printing'] += $panjang;
+                        }
+                    }
+                }
+            }
+        }
+        
+        // 4. Gudang Jadi (Packing)
+        $qPacking = \common\models\ar\TrnGudangJadi::find()
+            ->where(['>=', 'status', \common\models\ar\TrnGudangJadi::STATUS_STOCK])
+            ->andWhere(['between', 'date', $startDate, $endDate])
+            ->with(['wo.scGreige'])
+            ->all();
+            
+        foreach ($qPacking as $gj) {
+            $panjang = 0;
+            // Need to sum from items if available or just use qty
+            $panjang += $gj->qty; // Assumed qty is meters
+            
+            if ($gj->source == \common\models\ar\TrnGudangJadi::SOURCE_MAKLOON_PROSES || $gj->source == \common\models\ar\TrnGudangJadi::SOURCE_MAKLOON_FINISH) {
+                $data['packing_makloon'] += $panjang;
+            } else {
+                $process = ($gj->wo && $gj->wo->scGreige) ? $gj->wo->scGreige->process : null;
+                if ($process == \common\models\ar\TrnScGreige::PROCESS_PRINTING) {
+                    $data['packing_printing'] += $panjang;
+                } else {
+                    $data['packing_dyeing'] += $panjang;
+                }
+            }
+            $data['packing_total_packing'] += $panjang;
+            $data['hisaka_jml_masuk_packing'] += $panjang;
+            $data['packing_hasil_kirim'] += $panjang;
+        }
+
+        // Calculations
+        // Hari kerja in month (excluding Sundays)
+        $workdays = 0;
+        for ($i = 1; $i <= date('t', strtotime($startDate)); $i++) {
+            if (date('N', strtotime("$tahun-$bulan-$i")) < 7) $workdays++;
+        }
+        $data['hisaka_hari_kerja'] = $workdays;
+        
+        if ($workdays > 0) {
+            $data['hisaka_rata_per_hari_batch'] = round($data['hisaka_total_batch'] / $workdays);
+            $data['hisaka_rata_per_hari_kartu'] = round(count($hisakaKartuIds) / $workdays);
+            $data['hisaka_rata_per_hari_jumbo'] = round(count($hisakaJumboKartuIds) / $workdays);
+        }
+
+        return $this->render('rekap-bulanan-processing', [
+            'bulan' => $bulan,
+            'tahun' => $tahun,
+            'data' => $data
+        ]);
     }
     
     public function actionUpdatePersiapanGabungan()
